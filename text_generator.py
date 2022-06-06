029f1c6101e5cf91884745359e9e3cf71ed2a366
Amean <amean.asad1999@gmail.com>
2022-06-06 10:51:59 -0400

    Linting and lib updates

--- text_generator.py
+++ text_generator.py
@@ -8,8 +8,8 @@ Created on Sun Mar 10 15:55:25 2019
 import csv
 import random
 
-word_file =  open('words.csv', 'rt', encoding='utf')
-reader = csv.reader(word_file) 
+word_file = open('words.csv', 'rt', encoding='utf')
+reader = csv.reader(word_file)
 
 
 words_dictionary = {}
@@ -17,17 +17,15 @@ for row in reader:
     word = row[1].strip()
     word = word.replace("'", "")
     word = word.replace("-", "")
-    word = word.replace("/","")
+    word = word.replace("/", "")
     words_dictionary[row[0]] = word
-
 
 
 def randomized_text_generator(word_number):
     string = ''
     for word in range(word_number):
-        word_index = random.randint(2,4999)
+        word_index = random.randint(2, 4999)
         word = words_dictionary[str(word_index)].strip()
         string = word + " " + string
 
-             
     return string
